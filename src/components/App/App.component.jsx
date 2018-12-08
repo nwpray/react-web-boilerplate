592448360a4a58be3dba43b2ae1bf21509438694
Nick Pray <nickwpray@gmail.com>
2018-12-08 15:59:54 -0700

    feat: adds custom logger for immutable

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
+import Immutable from 'immutable';
 
 import Router from '@/components/Router';
 import { reducer } from '@/store';
+
+const logger = createLogger({
+  stateTransformer: state =>
+    Object.keys(state).reduce(
+      (newState, key) => ({
+        ...newState,
+        [key]: Immutable.Iterable.isIterable(state[key]) ? state[key].toJS() : state[key]
+      }),
+      {}
+    )
+});
 
 const store = createStore(reducer, applyMiddleware(logger));
 
