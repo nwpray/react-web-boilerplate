646abdd144a45c5aa40c569d1ce8a33e4a0fd3d1
Nick Pray <nickwpray@gmail.com>
2025-05-24 10:14:59 -0600

    updates all packages

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
-import '@babel/polyfill';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from '@/App.component';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
